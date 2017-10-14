9b9a9fcc19e901ea33db651ac1252e28c2f14e24
Felipe Silva Felix <fsfelix@ime.usp.br>
2017-10-14 17:27:51 -0300

    sync

--- code/extract_features_1.py
+++ code/extract_features_1.py
@@ -14,7 +14,7 @@ def generate_local_feature(file_dir, feat_name, feat_func, **kwargs):
         print('Loading {}...'.format(file_dir))
         y, sr = librosa.load(file_dir)
         print('generating {} for {}...'.format(feat_name, file_dir))
-        feature = feat_func(y = y, sr = sr, **kwargs)
+        feature = feat_func(y = y, **kwargs)
         np.savetxt(output_file, feature)
     else:
        print('{} already exists.'.format(output_file))
@@ -29,22 +29,25 @@ def extract_feat(data_dirs):
                     file_dir = subdir + '/' + file
 
                     #y, sr = librosa.load(file_dir)
-                    kwargs = {'min_dur' : 0.01, 'max_dur' : 3}
 
-                    generate_local_feature(file_dir, 'rmse', librosa.feature.rmse, y)
+                    kwargs = {}
 
-                    generate_local_feature(file_dir, 'stft', librosa.core.stft, y)
+                    generate_local_feature(file_dir, 'rmse', librosa.feature.rmse, **kwargs)
 
-                    generate_local_feature(file_dir, 'mfcc', librosa.feature.mfcc, y, **kwargs)
+                    generate_local_feature(file_dir, 'stft', librosa.core.stft, **kwargs)
 
-                    generate_local_feature(file_dir, 'spec_cent', librosa.feature.spectral_centroid, y, **kwargs)
+                    generate_local_feature(file_dir, 'mfcc', librosa.feature.mfcc, **kwargs)
 
-                    generate_local_feature(file_dir, 'spec_band', librosa.feature.spectral_bandwidth, y, **kwargs)
+                    generate_local_feature(file_dir, 'spec_cent', librosa.feature.spectral_centroid, **kwargs)
 
-                    generate_local_feature(file_dir, 'spec_roll', librosa.feature.spectral_rolloff, y, **kwargs)
+                    generate_local_feature(file_dir, 'spec_band', librosa.feature.spectral_bandwidth, **kwargs)
 
-                    generate_local_feature(file_dir, 'syllable_dur', esd.get_syllable_durations, **kwargs)
-                    generate_local_feature(file_dir, 'syllable_dur_list', esd.get_syllable_durations_list, **kwargs)
+                    generate_local_feature(file_dir, 'spec_roll', librosa.feature.spectral_rolloff, **kwargs)
+
+                    # kwargs = {'sr':  'min_dur' : 0.01, 'max_dur' : 3}
+                    # generate_local_feature(file_dir, 'syllable_dur', esd.get_syllable_durations, **kwargs)
+
+                    # generate_local_feature(file_dir, 'syllable_dur_list', esd.get_syllable_durations_list, **kwargs)
 
 #pool = Pool(processes = 4)
 #pool.map(extract_feat, util.DATA_DIR_POOL)
